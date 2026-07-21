d53b92cf82467de08dd4c7677185c75924cfa4ab
t.kaseke@alustudent.com <t.kaseke@alustudent.com>
2026-07-22 01:51:17 +0200

    feat(firebase): initialize Firebase on app startup

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
@@ -7,6 +8,7 @@ import 'injection/injection.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   await initDependencies();
 
   SystemChrome.setSystemUIOverlayStyle(
