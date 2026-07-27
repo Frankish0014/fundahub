402a18641905c31f26795b318fa366f87264be78
armeleger <a.kayisire@alustudent.com>
2026-07-27 09:10:37 +0200

    chore: add Firebase deps and initialize Firebase
    Add firebase_core + cloud_firestore and call Firebase.initializeApp in main.

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,15 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
 import 'app.dart';
 import 'core/theme/app_colors.dart';
+import 'firebase_options.dart';
 import 'injection/injection.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   await initDependencies();
 
   SystemChrome.setSystemUIOverlayStyle(
